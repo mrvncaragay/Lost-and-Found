4ce6f7782844fd0205e7c4965b4d5d9a308a5236
Marvin Caragay <mrvncaragay@gmail.com>
2019-06-15 20:24:22 -0700

    added semantic-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form>
+      <Form.Field>
+        <label>First Name</label>
+        <input placeholder="First Name" />
+      </Form.Field>
+      <Form.Field>
+        <label>Last Name</label>
+        <input placeholder="Last Name" />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label="I agree to the Terms and Conditions" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 }
 
