a80b4dbf49f03a1efa6c83a73b3154726cc08519
Marvin Caragay <mrvncaragay@gmail.com>
2019-06-15 21:41:46 -0700

    switch to bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import Nav from './Nav';
 
 function App() {
-  return (
-    <Form>
-      <Form.Field>
-        <label>First Name</label>
-        <input placeholder="First Name" />
-      </Form.Field>
-      <Form.Field>
-        <label>Last Name</label>
-        <input placeholder="Last Name" />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label="I agree to the Terms and Conditions" />
-      </Form.Field>
-      <Button type="submit">Submit</Button>
-    </Form>
-  );
+  return <Nav />;
 }
 
 export default App;
